1ca05f5033ab23944e96ff89e85a3fda9fb84690
Denis <ivdenvas@gmail.com>
2022-11-19 12:35:53 +0300

    modified main

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -2,22 +2,11 @@
 // причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
 Console.Clear();
-int X, Y;
+Console.Write("Введите X: ");
+int X = int.Parse(Console.ReadLine() ?? "");
 
-try
-{
-    Console.Write("Введите X: ");
-    X = int.Parse(Console.ReadLine() ?? "");
-
-    Console.Write("Введите Y: ");
-    Y = int.Parse(Console.ReadLine()?? "");
-}
-catch(Exception exc)
-{
-    Console.WriteLine("Ошибка ввода данных!  {exc.Message}");
-    return;
-}
-
+Console.Write("Введите Y: ");
+int Y = int.Parse(Console.ReadLine());
 if (X > 0 && Y > 0)
     Console.Write("1");
 else if (X < 0 && Y > 0)

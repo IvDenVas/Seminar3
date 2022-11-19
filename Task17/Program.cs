f8724d7b92971157f8a096b4074885d0170d1609
Denis <ivdenvas@gmail.com>
2022-11-19 12:10:32 +0300

    delete {}

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -8,22 +8,12 @@ int X = int.Parse(Console.ReadLine() ?? "");
 Console.Write("Введите Y: ");
 int Y = int.Parse(Console.ReadLine());
 if (X > 0 && Y > 0)
-{
     Console.Write("1");
-}
 else if (X < 0 && Y > 0)
-{
     Console.Write("2");
-}
 else if (X < 0 && Y < 0)
-{
     Console.Write("3");
-}
 else if (X > 0 && Y < 0)
-{
     Console.Write("4");
-}
 else 
-{
     Console.Write("Ошибка! Точка попадает на оси координат!");
-}
